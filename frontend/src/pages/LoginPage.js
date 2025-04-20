3bb236355c57e5b2b535cb92b5e6ea63b70b8116
noreyni03 <onoreyni03@gmail.com>
2025-04-20 21:17:02 +0000

    feat(application): Ajout de la page d'inscription.

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoginPage = () => {
+  return <h2>Page de Connexion (à implémenter)</h2>;
+};
+
+export default LoginPage;
---
+++ frontend/src/pages/RegistrationPage.js
@@ -0,0 +1,14 @@
+// frontend/src/pages/RegistrationPage.js
+import React from 'react';
+import RegistrationForm from '../components/Auth/RegistrationForm';
+
+const RegistrationPage = () => {
+  return (
+    <div>
+      <h1>Inscription</h1>
+      <RegistrationForm />
+    </div>
+  );
+};
+
+export default RegistrationPage;
