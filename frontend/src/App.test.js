2adc213118ef5fcfa32b9a0fb812dbe0a68e1c3c
noreyni03 <onoreyni03@gmail.com>
2025-04-20 21:57:53 +0000

    update(ci-cd): modification de App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,26 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App'; 
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  BrowserRouter: ({ children }) => <div>{children}</div>,
+  
+  Link: ({ children, to }) => <a href={to}>{children}</a>,
+  Routes: ({ children }) => <div>{children}</div>, // Mock simple pour Routes
+  Route: ({ element }) => element, // Mock simple pour Route, rend l'élément passé
+}));
+
+test('renders navigation links', () => {
+  render(<App />); // Rend le composant App avec le routeur moqué
+
+  const homeLink = screen.getByText(/Accueil/i);
+  expect(homeLink).toBeInTheDocument();
+
+  const registerLink = screen.getByText(/Inscription/i);
+  expect(registerLink).toBeInTheDocument();
+
+  const loginLink = screen.getByText(/Connexion/i);
+  expect(loginLink).toBeInTheDocument();
+
 });
