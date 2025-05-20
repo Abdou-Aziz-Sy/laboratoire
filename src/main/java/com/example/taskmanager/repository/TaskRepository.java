2ebd6023ae144146345acae30249f4e3e5e9904c
japhet-mokoumbou <japhetmokoumbousekolet@gmail.com>
2025-05-20 12:45:08 +0000

    API de récupération des tâches

--- src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -2,11 +2,13 @@ package com.example.taskmanager.repository;
 
 import com.example.taskmanager.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
     List<Task> findByUserId(Long userId);
+    List<Task> findAll();
 }
