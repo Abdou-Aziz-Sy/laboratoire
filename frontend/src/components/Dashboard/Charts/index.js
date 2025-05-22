296fd13cce37f0f3cfa87a61644426ab9d208b7f
noreyni03 <onoreyni03@gmail.com>
2025-05-22 15:12:35 +0100

    feat(UI) :Ajout du dashboard

---
+++ frontend/src/components/Dashboard/Charts/index.js
@@ -0,0 +1,5 @@
+// --- fichier: frontend/src/components/Dashboard/Charts/index.js ---
+// Exporte tous les composants de graphiques pour faciliter leur importation
+
+export { default as TaskProgressChart } from './TaskProgressChart';
+export { default as ActivityTimelineChart } from './ActivityTimelineChart';
